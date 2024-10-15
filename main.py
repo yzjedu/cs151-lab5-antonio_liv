4f4742f22a79ecbbe6aef118dcedcad1e6ddef37
ajduenomartinez <ajduenomartinez@loyola.edu>
2024-10-15 09:49:57 -0400

    main intro comments

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# Programmers:
+# Programmers: Antonio Dueno, Liv Oakes
 # Course:  CS151, Dr. Zelalem Jembre Yalew
-# Due Date: 10//24
+# Due Date: 10/16/24
 # Lab Assignment: 5
-# Problem Statement:
-# Data In:
-# Data Out:
+# Problem Statement: Build a simulation of an ATM where the user can check their balance, withdraw money, and deposit money.
+# Data In: user input if they wish to withdraw, how much they wish to withdraw, deposit, how much they wish to deposit,
+#          view balance, or exit ATM.
+# Data Out: Total account balance after depositing, total account balance after withdrawing, total account balance
 # Credits:In class
